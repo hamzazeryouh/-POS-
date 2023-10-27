4f22ec7189a80b8deeb65da0d2608f5c8fd47aa9
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-10-27 15:15:30 +0200

    Ajoutez des fichiers projet.

---
+++ (POS)/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _POS_
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ (POS)/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _POS_
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
